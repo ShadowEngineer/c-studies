272f7f211691c6e37e1244b10825931ff261265f
ShadowEngineer <ShadowEngineer54@gmail.com>
2022-09-21 11:21:25 +0100

    blank counter

---
+++ chapter-1/blank_counter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, b, t, nl = 0;
+
+    while ((c = getchar()) != EOF) {
+        switch (c)
+        {
+            case ' ':
+                ++b;
+                break;
+            case '\t':
+                ++t;
+                break;
+            case '\n':
+                ++nl;
+                break;
+        }
+    }
+
+    printf("Blanks:\t\t%d\nTabs:\t\t%d\nNew lines:\t%d\n", b, t, nl);
+}

034f711c4b9e7177aab44cdcdc7a72f1b51cac18
ShadowEngineer <ShadowEngineer54@gmail.com>
2022-09-21 11:01:52 +0100

    add char counter 2nd vers

--- chapter-1/char_counter.c
+++ chapter-1/char_counter.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-/* count characters in input; 1st version */
+/* count characters in input */
 int main()
 {
+    /* 1st version
     long nc;
 
     nc = 0;
     while (getchar() != EOF)
         ++nc;
     printf("\n%1li\n", nc);
+    */
+
+    double nc;
+
+    for (nc = 0.0; getchar() != EOF; ++nc)
+        ; // null statement since for loops must have a body
+    printf("\n%.0f\n", nc);
 }

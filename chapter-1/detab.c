464f0f15fbc86ee598919e209a1d9f0b59bc1e8c
ShadowEngineer <ShadowEngineer54@gmail.com>
2022-09-23 09:30:43 +0100

    start detab

---
+++ chapter-1/detab.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define TAB_COLUMN_WIDTH 8      // the maximum number of blanks equivalent to a tab
+
+int main(int argc, char **argv)
+{
+    int c;
+    while ((c = getchar()) != EOF)
+        putchar(c);
+    putchar('\n');
+}

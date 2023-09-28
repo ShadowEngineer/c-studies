4d57792f1753e1d7f763cc57f29c06c6b3879faf
ShadowEngineer <ShadowEngineer54@gmail.com>
2023-09-28 11:54:24 +0100

    document runner

--- runner.sh
+++ runner.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# compiles and executes the given C source file.
 run() {
     if [[ -e $1 ]]; then
         clear
         gcc $1 -o output
         shift
+
+        # test inputs can be given to the file to test various features if the test inputs are massive.
         if [[ $1 == "-c" ]]; then
             ./output $@
         else
